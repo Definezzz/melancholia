6d532301d3680b1bb1b0a717e5162d5c70768354
definezzz <107827720+Definezzz@users.noreply.github.com>
2024-06-17 20:42:01 +0500

    Update saveui.lua

--- saveui.lua
+++ saveui.lua
@@ -12,7 +12,7 @@ if not CopyToClipboard then
 end
 
 local SaveManager = {} do
-	SaveManager.Folder = getgenv().settings_folder or "MelancholiaSettings"
+	SaveManager.Folder = getgenv().settings_folder or "melancholia"
 	SaveManager.Ignore = {}
 	SaveManager.Parser = {
 		Toggle = {
